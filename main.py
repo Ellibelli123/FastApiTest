9f4a9918d98f01d47623086d9d54a00feb441be8
Meiyi Fok <meiyi.fok1@gmail.com>
2026-02-20 09:20:20 +0100

    här kommer ny kod

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
+
+
 
 @app.get("/")
 def root():
     return {"status": "ok", "message": "Hello FastAPI"}
+
+
+@app.get("/magnus")
+def root():
+    return {"status": "ok", "message": "Magnus Hello FastAPI"}
+
+
+@app.get("/bookings")
+def root():
+    return {"status": "ok", "message": "Magnus Hello FastAPI"}
+
+
+@app.put("/bookings")
+def root():
+    return {"status": "ok", "message": "Magnus Hello FastAPI"}
